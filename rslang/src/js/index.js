394b6c0e56044cf2b50aea54e1718b353d190e71
Olga Studzenichnik <okrypets@gmail.com>
2020-06-20 01:48:45 +0300

    feat:add authorization mechanics without style

--- rslang/src/js/index.js
+++ rslang/src/js/index.js
@@ -1,4 +1,15 @@
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
+// eslint-disable-next-line import/named
+import { authorization, logIn } from '../authorization-page/js/authorization';
 
-import '../assets/scss/style.scss';
+if (typeof window !== 'undefined') {
+  window.onload = () => {
+    if (localStorage.getItem('SWAuthData') && JSON.parse(localStorage.getItem('SWAuthData')).userId) {
+      logIn();
+      // renderHomePage();
+    } else {
+      authorization();
+    }
+  };
+}
