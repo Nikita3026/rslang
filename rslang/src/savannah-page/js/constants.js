27afbbbe2d8919ed88ee4db9096d8de4b4d73c63
Nikita Mihadyuk <mihadyuk.nikita@gmail.com>
2020-06-20 13:55:17 +0300

    feat:add loading page layout

--- rslang/src/savannah-page/js/constants.js
+++ rslang/src/savannah-page/js/constants.js
@@ -2,6 +2,9 @@ const constants = {
     BTN_START: document.querySelector('.btn-start'),
     START_PAGE: document.querySelector('.savannah-start-page'),
     MAIN_PAGE: document.querySelector('.savannah-game-page'),
+    LOADING_PAGE: document.querySelector('.page-loading'),
+    COUNTDOWN: document.querySelector('.countdown'),
+    MUTE_BTN: document.querySelector('.mute-sounds'),
 }
 
 export default constants;
--- rslang/src/savannah-page/js/savannah.js
+++ rslang/src/savannah-page/js/savannah.js
@@ -6,5 +6,19 @@ import '../../assets/scss/style.scss';
 
 constants.BTN_START.addEventListener('click', () => {
     constants.START_PAGE.hidden = true;
-    constants.MAIN_PAGE.hidden = false;
+    constants.LOADING_PAGE.hidden = false;
+    const countdownInterval = setInterval(() => {
+        if (constants.COUNTDOWN.innerText == 1) {
+            clearInterval(countdownInterval);
+            constants.LOADING_PAGE.hidden = true;
+            constants.MAIN_PAGE.hidden = false;
+            constants.COUNTDOWN.innerText = 3;
+        } else {
+            constants.COUNTDOWN.innerText = +constants.COUNTDOWN.innerText - 1;
+        }
+    }, 1000)
+})
+
+constants.MUTE_BTN.addEventListener('click', () => {
+    constants.MUTE_BTN.classList.toggle('muted');
 })
