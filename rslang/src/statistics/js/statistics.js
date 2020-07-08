6d9bb83f840001dfabb3b03a43efe8cb87640d9d
TimofeiShpak <tima.shpak.v@gmail.com>
2020-07-08 11:21:58 +0400

    feat: add text statistics

--- rslang/src/statistics/js/statistics.js
+++ rslang/src/statistics/js/statistics.js
@@ -2,6 +2,9 @@ import Chart from 'chart.js';
 
 const elementNumberWords = document.querySelector('.number-words');
 const speedCanvas = document.getElementById('speedChart');
+const series = document.querySelector('.series');
+const cards = document.querySelector('.cards');
+const rightAnswer = document.querySelector('.right-answer');
 
 const MIN_NUMBER = 0;
 const NEXT_NUMBER = 1;
@@ -18,7 +21,17 @@ saveStatistic['начало использования приложения'] = 
 let days = [];
 let words = [];
 let numberWords = 0;
-delete saveStatistic.seria;
+
+function createStatisticsText() {
+  const proportionRightAnswer = saveStatistic.rightAnswer / saveStatistic.cards;
+  series.innerText = `Общее количество серий прохождения карточек: ${saveStatistic.seria}`;
+  cards.innerText = `Общее количество пройденных карточек: ${saveStatistic.cards}`;
+  rightAnswer.innerText = `Процент правильных ответов: ${proportionRightAnswer * 100}%`;
+  delete saveStatistic.seria;
+  delete saveStatistic.cards;
+  delete saveStatistic.rightAnswer;
+}
+createStatisticsText();
 
 function createDataWords() {
   const statistics = Object.entries(saveStatistic);
@@ -45,7 +58,7 @@ function createLabels() {
 const speedData = {
   labels: createLabels(),
   datasets: [{
-    label: 'Изученные слова',
+    label: 'Изучаемые слова',
     data: words,
     borderColor: 'rgba(255,45,45,1)',
     backgroundColor: 'rgba(255,90,90,0.5)',
