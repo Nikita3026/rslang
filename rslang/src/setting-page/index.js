ff0b14a092f4563459fe2cfbd4598e1493607f5e
Anna <anna_agabekyan_anna@mail.ru>
2020-07-12 22:46:39 +0400

    fix conflict

--- rslang/src/setting-page/index.js
+++ rslang/src/setting-page/index.js
@@ -2,21 +2,16 @@ import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 import 'bootstrap';
 import { renderSideBar } from '../js/SideBar/SideBar';
-import {
-  setBodyDataToDom, checkTokenIsValid, routeTo, updateToken,
-} from '../js/helpers';
+import { setBodyDataToDom, checkTokenIsValid, routeTo } from '../js/helpers';
 
 import './css/style.scss';
 
 window.onload = async () => {
-  if (localStorage.getItem('SWAuthData')) {
-    if (!checkTokenIsValid()) {
-      updateToken();
-    }
+  if (localStorage.getItem('SWAuthData') && checkTokenIsValid()) {
     await setBodyDataToDom('setting-page.html');
     renderSideBar();
-    const { renderApp } = await import('./js/setting-page');
-    renderApp();
+    const { setSettingsLocalstorage } = await import('./js/setting-page');
+    setSettingsLocalstorage();
   } else {
     routeTo('/authorization');
   }
--- rslang/webpack.config.js
+++ rslang/webpack.config.js
@@ -1,17 +1,10 @@
 const HtmlWebPackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const CopyWebPackPlugin = require('copy-webpack-plugin');
 const path = require('path');
 
-
-
 module.exports = {
-<<<<<<< HEAD
-    watch: true,
-    entry: {
-        index: './src/card-page/js/index.js',
-=======
   watch: true,
   entry: {
     index: './src/index.js',
@@ -40,75 +33,34 @@ module.exports = {
           presets: ['@babel/preset-env'],
         },
       },
->>>>>>> develop
     },
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: '[name].js',
+    {
+      test: /\.html$/,
+      use: [{
+        loader: 'html-loader',
+        options: { minimize: true },
+      }],
     },
-    module: {
-        rules: [{
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader",
-                    options: {
-                        presets: ['@babel/preset-env']
-                    }
-                }
-            },
-            {
-                test: /\.html$/,
-                use: [{
-                    loader: "html-loader",
-                    options: { minimize: true }
-                }]
-            },
-            {
-                test: /\.(png|svg|jpe?g|gif)$/,
+    {
+      test: /\.(png|svg|jpe?g|gif)$/,
 
-                use: [{
-                    loader: "file-loader",
-                    options: {
-                        name: '[name].[ext]',
-                        outputPath: 'assets/images/',
-                    }
-                }]
-            },
-            {
-                test: /\.scss$/,
-                use: [
-                    "style-loader",
-                    "css-loader",
-                    "sass-loader"
-                ]
-            },
-        ]
+      use: [{
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]',
+          outputPath: 'assets/images/',
+        },
+      }],
     },
-    plugins: [
-        new MiniCssExtractPlugin({
-            filename: "style.css",
-            chunkFilename: "[id].css"
-        }),
-        new CopyWebPackPlugin({
-            patterns: [
-                { from: 'src/assets/images/', to: './assets/images' }
-            ]
-        }),
-        new HtmlWebPackPlugin({
-            filename: 'index.html',
-            template: './src/card-page/index.html',
-            chunks: ['index']
-        }),
-        new CleanWebpackPlugin(),
+    {
+      test: /\.scss$/,
+      use: [
+        'style-loader',
+        'css-loader',
+        'sass-loader',
+      ],
+    },
     ],
-<<<<<<< HEAD
-    devServer: {
-        port: 5500,
-        contentBase: path.join(__dirname, 'dist')
-    }
-};
-=======
   },
   plugins: [
     new MiniCssExtractPlugin({
@@ -182,4 +134,3 @@ module.exports = {
     contentBase: path.join(__dirname, 'dist'),
   },
 };
->>>>>>> develop
