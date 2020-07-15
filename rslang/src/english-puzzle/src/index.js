f5aaf6fb0730d14eee6598e94379701e37901800
Alswede <alexander.alswede@gmail.com>
2020-07-15 05:30:54 +0100

    docs: удаление комментария

--- rslang/src/english-puzzle/src/index.js
+++ rslang/src/english-puzzle/src/index.js
@@ -2,7 +2,6 @@ import { createSignPage, createStartPage } from './js/create';
 import './js/dragHandler';
 
 window.onload = () => {
-  // signIN({ email: 'o@user.com', password: 'Gfhjkm_123' });
   const token = localStorage.getItem('token');
   if (token) {
     createStartPage();
